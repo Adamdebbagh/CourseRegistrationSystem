a264506907f1d0dd3856ab3c5e2f377e83666b96
Adam debbagh <adamdebbagh@gmail.com>
2015-03-06 13:40:42 -0500

    7.implemented acknowledgment class

--- src/courseregistration/project/Acknowledgment.java
+++ src/courseregistration/project/Acknowledgment.java
@@ -1,9 +1,30 @@
 package courseregistration.project;
 
-public class Acknowledgment {
+import java.util.Date;
+import java.util.List;
 
-	public Acknowledgment() {
-		// TODO Auto-generated constructor stub
+import courseregistration.project.Course.PreRequite;
+
+public class Acknowledgment {
+	String result; 
+	double amountDue;
+	 Date time; 
+	 String room;
+	 String courseNumber; 
+	 double totalTuition; 
+	 String courseClosed;
+	 List<PreRequite> prerequisites;
+	public Acknowledgment() {}
+	
+	@SuppressWarnings("unused")
+	private void showResult(boolean status){
+		if(status == true){
+		result = RegistrationManager.registrationStatus[0];
+		// construct the other params here
+		}else{
+			result = RegistrationManager.registrationStatus[1];	
+			// construct the other params here
+		}
 	}
 
 }
