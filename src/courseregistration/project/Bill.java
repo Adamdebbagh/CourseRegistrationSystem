895c1ea0ec0de8424051dc0e15298cf07ec00220
Adam debbagh <adamdebbagh@gmail.com>
2015-03-06 13:41:06 -0500

    8.implemented bill class

--- src/courseregistration/project/Bill.java
+++ src/courseregistration/project/Bill.java
@@ -2,8 +2,19 @@ package courseregistration.project;
 
 public class Bill {
 
-	public Bill() {
-		// TODO Auto-generated constructor stub
-	}
 
+	public Bill() {}
+	
+	@SuppressWarnings("unused")
+	private void sendBill(String studentName, int ssn) {
+		Student student = new Student(studentName, ssn);
+		String name = student.getStudentName();
+		String address = student.getAddress(name);
+		double fee = student.getFee(); 
+		
+		System.out.println("Bill sent to  ");
+		System.out.println("Student Name : "+ name);
+		System.out.println("Adreess :" + address);
+		System.out.println("fee Owed :" +fee);
+	}
 }
