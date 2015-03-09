54f3f0d73b928929ca10a66b5fd027356ead3119
Adam Debbagh <adamdebbagh@gmail.com>
2015-03-09 16:31:29 -0400

    Update Class.java

--- src/courseregistration/project/Class.java
+++ src/courseregistration/project/Class.java
@@ -2,43 +2,47 @@ package courseregistration.project;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Class {
-	
-	protected static final int classSize = 30;
-	protected int position = 0;
-	protected boolean full = false ;
-	
-	protected List<Student> studentsList = new ArrayList<Student>();
-	
-	public Class() {}
-	
-	// if student successfully checked. add him to class list
-	@SuppressWarnings("unused")
-	private void addStudentToList(Student student){
-		if(studentsList.size() <= classSize){
-			studentsList.add(student);
-			position++;
-			System.out.println("stuent current position in the class : ");
-		}
-		else{
-			full = true;
-			System.out.println("Class is full. No more student can be added :\n full : "+full );
-		}
-	}
-	@SuppressWarnings("unused")
-	private boolean isPositionAvailable() {
-		if(position <= classSize){
-			return true;
-			}
-		return false;
-	}
-	@SuppressWarnings("unused")
-	private boolean isClassFull(){
-		if(full == true){
-			return true;
-			}
-		return false;
-	}
-	
 
+public class Class {
+	 
+    protected static final int classSize = 30;
+    protected int position = 0;
+    protected boolean full = false ;
+
+    protected List<Student> studentsClassList = new ArrayList<Student>();
+    protected List<String> studentsClassSSNList = new ArrayList<String>();
+
+    public Class() {}
+
+    // if student successfully checked. add him to class list
+    protected void addStudentToClass(Student student){
+        if(studentsClassList.size() < classSize){
+            if(!studentsClassSSNList.contains(student.getStudentSSN())){
+                studentsClassList.add(student);
+                position++;
+                System.out.println("student current position in the class : "+ position);
+                studentsClassSSNList.add(student.getStudentSSN());
+            }else{
+                System.out.println(student.getStudentName()+" already registered to the class.");
+            }
+        }
+        else{
+                full = true;
+                System.out.println("Class is full. No more student can be added :\n full : "+full );
+        }
+    }
+
+    protected boolean isPositionAvailable() {
+        if(position < classSize){
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean isClassFull(){
+        if(full == true){
+            return true;
+       }
+       return false;
+    }
 }
