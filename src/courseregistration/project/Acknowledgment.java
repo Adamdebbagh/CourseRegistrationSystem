e40eb02863af01e8b40eb292fb1cd3890b8e2231
Adam Debbagh <adamdebbagh@gmail.com>
2015-03-09 16:34:18 -0400

    Update Acknowledgment.java

--- src/courseregistration/project/Acknowledgment.java
+++ src/courseregistration/project/Acknowledgment.java
@@ -1,30 +1,39 @@
 package courseregistration.project;
 
-import java.util.Date;
-import java.util.List;
 
+import java.util.List;
 import courseregistration.project.Course.PreRequite;
+import java.util.Arrays;
 
 public class Acknowledgment {
-	String result; 
-	double amountDue;
-	 Date time; 
-	 String room;
-	 String courseNumber; 
-	 double totalTuition; 
-	 String courseClosed;
-	 List<PreRequite> prerequisites;
-	public Acknowledgment() {}
-	
-	@SuppressWarnings("unused")
-	private void showResult(boolean status){
-		if(status == true){
-		result = RegistrationManager.registrationStatus[0];
-		// construct the other params here
-		}else{
-			result = RegistrationManager.registrationStatus[1];	
-			// construct the other params here
-		}
-	}
 
+    List<PreRequite> prerequisites;
+    
+    public Acknowledgment() {}
+
+    protected void showResult(String course,String day,String time,String room,double tuition){
+        System.out.println("\n ACKNOWLEDGMENT");
+        System.out.println("\n \n Thank you for registering for " + course +" course\n");
+        System.out.println("\n Calss Details :");
+        System.out.println("The Course will lauch on: " + day + "at "+ time);
+        System.out.println(" The course will be taken place at room : "+ room);
+        System.out.println(" The current course tuition is: "+ tuition);
+        System.out.println("\n We wish you good learning Experience! ");	
+    }
+    protected void showAcknowlegement(String name,double feeOwed){
+        System.out.println("\n ACKNOWLEDGMENT \n");
+        System.out.println(name +" cannot be registered until all fees are paid");
+        System.out.println("Amount due is : "+ feeOwed);
+    }
+    protected void showAcknowlegement(String name, String courseSelected, String[] studentTranscript, String coursePrerequite) {
+        System.out.println("\n ACKNOWLEDGMENT \n");
+        System.out.println(name+" picked : "+ courseSelected);
+        System.out.println(name+"'s transcript  : "+ Arrays.toString(studentTranscript));
+        System.out.println("the course : " + courseSelected +","+" has the following preRequisite : "+ coursePrerequite);
+        System.out.println(name+" can not be registered until prequisites are filled");
+}
+    protected void showAcknowlegement(String message){
+        System.out.println("\n ACKNOWLEDGMENT \n");
+        System.out.println(message);
+    }
 }
