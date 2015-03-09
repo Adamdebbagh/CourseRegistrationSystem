84f02605ceaca839332230dd81b0e2b58b33f9a0
Adam Debbagh <adamdebbagh@gmail.com>
2015-03-09 16:13:05 -0400

    Update RegistrationSystem.java

--- src/courseregistration/project/RegistrationSystem.java
+++ src/courseregistration/project/RegistrationSystem.java
@@ -2,20 +2,12 @@ package courseregistration.project;
 
 public class RegistrationSystem {
 	
-	static RegistrationForm form = new RegistrationForm();
-	static RegistrationManager rm = new RegistrationManager();
-	static AccountsReceivable accountFee = new AccountsReceivable();
-	public RegistrationSystem() {}
-
-	public static void main(String[] args) {
+    static RegistrationForm form = new RegistrationForm();
 	
-		// 1- on launch, show student a registration Form.
-		//    on form Submitted, send data to Registration Manager (clerk). 	
-		if(RegistrationForm.sent == false){	
-			form.showForm();
-		}
-		if(RegistrationForm.sent == true){	
-			System.out.println( "Checking Accounts Receivable for Student owed fees...");	
-		}	
+    public RegistrationSystem() {}
+    
+    public static void main(String[] args) {
+            // on launch, show student a registration Form.
+            form.showForm();                       
   }
 }
